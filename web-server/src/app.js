aa17c61978fd46fa39a86ce8c699aa66fb95ffff
OfirLevy <ofir.l@monolith.co.il>
2021-01-18 00:33:42 +0200

    test for production

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -12,6 +12,7 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public');
 app.use(express.static(publicDirectoryPath));
 app.use(express.json());
+app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
 app.get('/payments', (req, res,) => {
